ea705b0045972f176f15e0461666d915a7bc6802
unknown <ddung13701@gmail.com>
2020-09-30 10:22:59 +0700

    asm3

---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using FoodASM.Models;
+using FoodASM.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FoodASM
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static Frame mainFrame;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            mainFrame = MainFrame;
+        }
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            MN.Items.Add(new MenuItem((char)(Convert.ToInt32("E80F", 16)), "Home", "home"));
+            MN.Items.Add(new MenuItem((char)(Convert.ToInt32("EA4A", 16)), "Contact", "listContact"));
+            MN.Items.Add(new MenuItem((char)(Convert.ToInt32("E715", 16)), "Email", "listMail"));
+            MN.Items.Add(new MenuItem((char)(Convert.ToInt32("E77B", 16)), "Customer", "listCustomer"));
+        }
+        private void ListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MenuItem selected = (MenuItem)MN.SelectedItem;
+            if (selected.Des.Equals("home"))
+            {
+                MainFrame.Navigate(typeof(Home));
+            }
+            else if (selected.Des.Equals("listContact"))
+            {
+                MainFrame.Navigate(typeof(ListContact));
+            }
+            else if (selected.Des.Equals("listMail"))
+            {
+                MainFrame.Navigate(typeof(ListMail));
+            }
+            else if(selected.Des.Equals("listCustomer"))
+            {
+                MainFrame.Navigate(typeof(ListCustomer));
+            }
+        }
+        private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(Home));
+        }
+    }
+}
+
---
+++ Models/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodASM.Models
+{
+    class MenuItem
+    {
+        public char icon;
+        public string name;
+        public string des; 
+        public MenuItem(char icon, string name, string des)
+        {
+            Icon = icon;
+            Name = name;
+            Des = des;
+        }
+
+        public char Icon
+        {
+            get => icon;
+            set => icon = value;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Des
+        {
+            get => des;
+            set => des = value;
+        }
+    }
+}
